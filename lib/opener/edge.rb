611015c8e258f179315c72807df2b03030457812
Stephen Touset <stephen@touset.org>
2009-03-19 14:40:30 -0400

    Edge skeleton

---
+++ lib/opener/edge.rb
@@ -0,0 +1,29 @@
+require 'singleton'
+
+require 'facets/kernel/returning'
+
+class Opener::Edge
+  include Singleton
+  
+  EDGES = Hash.new {|h,k| h[k] = {} }
+  
+  attr_accessor :parent
+  attr_accessor :child
+  
+  def self.instance(parent, move)
+    EDGES[parent][move] ||= new(parent, move)
+  end
+  
+  def initialize(parent, move)
+    self.parent = parent
+    self.child  # = Opener::Node.new(parent.moves + [move])
+  end
+  
+  private
+  
+  def method_missing(move, name = nil, &block)
+    returning self.class.instance(self.node, move, &block) do |edge|
+      edge.child.name = name
+    end
+  end
+end
