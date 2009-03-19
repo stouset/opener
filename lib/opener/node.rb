ec2d16201193311c4202c8181f45ccb4b3aa6fdc
Stephen Touset <stephen@touset.org>
2009-03-19 15:59:42 -0400

    Lots more Node accessors

--- lib/opener/node.rb
+++ lib/opener/node.rb
@@ -6,12 +6,19 @@ class Opener::Node
   NODES = {}
   
   attr_accessor :id
+  attr_accessor :parents
+  attr_accessor :name
+  
+  attr_accessor :wins
+  attr_accessor :losses
+  attr_accessor :annotation
   
   def self.instance(id)
     NODES[id] ||= new(id)
   end
   
   def initialize(id)
-    self.id = id
+    self.id      = id
+    self.parents = []
   end
 end
