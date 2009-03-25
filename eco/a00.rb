c3648ce7d34ec786f55acd0036c4140ad488ff4a
Stephen Touset <stephen@touset.org>
2009-03-25 12:17:32 -0400

    A05 ECO codes

--- eco/a00.rb
+++ eco/a00.rb
@@ -38,7 +38,7 @@ g3('Benko\'s Opening') do
   c5  { self * 1533 + 35.7 - 28.6 }.Bg2
   
   ~ d5.Nf3.Nf6   # -> King's Indian Attack
-  ~ Nf6().Nf3.d5 # -> King's Indian Attack
+  ~ Nf6().Nf3    # -> King's Indian Attack
   ~ Nf6().c4.e5  # -> English
   ~ Nf6().Bg2.d5 # -> d5.Bg2.Nf6
   ~ g6.c4.Bg7    # -> English
---
+++ eco/a05.rb
@@ -0,0 +1,5 @@
+Nf3().Nf6.g3 do
+  b5('King\'s Indian Attack')
+  b5(:'Spassky\'s Variation')
+  g6('King\'s Indian Attack').b4('Réti-Smyslov Variation')
+end
