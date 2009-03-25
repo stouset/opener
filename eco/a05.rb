77dc5d9304bec4ef70629700d321f5d875c2a9b2
Stephen Touset <stephen@touset.org>
2009-03-25 12:33:10 -0400

    Statistics from Chess365.com

--- eco/a05.rb
+++ eco/a05.rb
@@ -1,6 +1,25 @@
-Nf3().Nf6.g3 do
-  b5('King\'s Indian Attack')
-  b5(:'Spassky\'s Variation')
-  g6.b4('King\'s Indian Attack')
-  g6.b4(:'Réti-Smyslov Variation')
+Nf3().Nf6 do
+  self * 85267 + 36.8 - 25.5
+  
+  g3 do
+    self * 24810 + 35.2 - 27.9
+    
+    b5('King\'s Indian Attack') * 996 + 29.6 - 32.7
+    b5(:'Spassky\'s Variation')
+    
+    g6 do
+      self * 12173 + 35.6 - 26.8
+      
+      b4('King\'s Indian Attack')
+      b4(:'Réti-Smyslov Variation')
+    end
+    
+    d5 * 6279 + 33.0 - 30.7
+    b6 * 2519 + 33.6 - 25.5
+    c5 * 1245 + 36.6 - 27.8
+  end
+  
+  c4 * 43799 + 38.4 - 21.8
+  d4 * 11056 + 35.7 - 30.1
+  b3 *  3934 + 35.2 - 31.8
 end
