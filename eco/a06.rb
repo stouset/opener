8ddf9906e745d6f248836f4bd5331e52bebb11ff
Stephen Touset <stephen@touset.org>
2009-03-26 15:46:00 -0400

    Stats from 365Chess.com

--- eco/a06.rb
+++ eco/a06.rb
@@ -1,5 +1,13 @@
-Nf3().d5("Reti Opening")
-Nf3().d5.d3(:"Old Indian attack")
-Nf3().d5.b4("Santasiere's folly")
-Nf3().d5.e4("Tennison (Lemberg, Zukertort) gambit")
-Nf3().d5.b3(:"Nimzovich-Larsen attack")
+Nf3().d5 do
+  self * 51967 + 37.7 - 26.9
+  
+  b4('Santasiere\'s Folly')                  * 194 + 32.5 - 38.7
+  e4('Tennison (Lemberg, Zukertort) Gambit') *  69 + 40.6 - 34.8
+  
+  d3(:'Old Indian Attack')       *  582 + 33.5 - 39.2
+  b3(:'Nimzovich-Larsen Attack') * 2873 + 36.4 - 30.1
+  
+  d4 { self * 17224 + 36.9 - 26.3 }.Nf6.c4
+  g3        * 17681 + 37.7 - 26.7
+  c4        * 12200 + 39.6 - 26.0
+end
