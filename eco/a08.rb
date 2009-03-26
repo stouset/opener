2229375206819e8d93fa2defc3deef4c7cc85a2e
Stephen Touset <stephen@touset.org>
2009-03-26 17:15:31 -0400

    Statistics from 365chess.com

--- eco/a08.rb
+++ eco/a08.rb
@@ -1,5 +1,12 @@
 Nf3().d5.g3.c5.Bg2 do
-  Nc6().O_O.e6.d3.Nf6.Nbd2.Be7.e4.O_O.Re1(:"French Variation")
-  Nc6().O_O.e6.d3.Bd6.e4.Nge7 | '='
-  g6.O_O.Bg7.d3.Nf6.Nbd2.Nc6  | '='
+  self * 2446 + 40.0 - 29.4
+  
+  Nc6 do
+    self * 2104 + 39.8 - 29.9
+    
+    O_O().e6.d3.Nf6.Nbd2.Be7.e4.O_O.Re1(:"French Variation")
+    O_O().e6.d3.Bd6.e4.Nge7 | '='
+  end
+  
+  g6 { self * 73 + 32.9 - 21.9 }.O_O.Bg7.d3.Nf6.Nbd2.Nc6  | '='
 end
