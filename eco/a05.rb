062363e3789cb432dd22f6fc93a4cfc798cfe0dd
Stephen Touset <stephen@touset.org>
2009-03-26 16:32:48 -0400

    Resolve transpositions

--- eco/a05.rb
+++ eco/a05.rb
@@ -10,11 +10,8 @@ Nf3().Nf6 do
       self * 12173 + 35.6 - 26.8
       
       b4(:'Réti-Smyslov Variation')
-      
-      Bg2().Bg7.O_O.O_O.d3.d5.Nbd2.Nc6 | '+='
     end
     
-    d5 { self * 6279 + 33.0 - 30.7 }.Bg2.c6.O_O.Bg4.d3.Nbd7.Nbd2.e5 | '='
     b6 * 2519 + 33.6 - 25.5
     c5 * 1245 + 36.6 - 27.8
   end
@@ -22,8 +19,9 @@ Nf3().Nf6 do
   b3 * 3934 + 35.2 - 31.8 | '='
   e3 *  508 + 35.0 - 32.7 | '='
   
-  ~ d4 * 11056 # -> Indian Defence
-  ~ c4 * 43799 # -> English Opening
-  ~ g3.g6.d4   # -> d4.Nf6.Nf3.g6.g3
-  ~ g3.g6.c4   # -> c4.Nf6.Nf3.g6.g3
+  ~ d4    * 11056 # -> Indian Defence
+  ~ c4    * 43799 # -> English Opening
+  ~ g3.d5 * 6279  # -> Nf3.d5.g3.Nf6
+  ~ g3.g6.d4      # -> d4.Nf6.Nf3.g6.g3
+  ~ g3.g6.c4      # -> c4.Nf6.Nf3.g6.g3
 end
