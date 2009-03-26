e9b4c2a58b6cbfa0c2b094d3c2ddbf3c2b093f3b
Stephen Touset <stephen@touset.org>
2009-03-26 16:08:08 -0400

    Statistics from 365chess.com

--- eco/a07.rb
+++ eco/a07.rb
@@ -1,7 +1,12 @@
 Nf3().d5.g3("King's Indian Attack", :'Barcza System') do
-  Nf6().Bg2.c6.O_O.Bg4(:"Yugoslav Variation")
-  Bg4().Bg2.Nd7(:"Keres Variation")
-  g6.Bg2.Bg7.O_O.e5.d3.Ne7(:"Pachman System")
-  c5(:"with ...c5")
-  c5.Bg2.Nc6.O_O.e6.d3.Nf6.Nbd2.Be7.e4.O_O.Re1(:"French Variation")
+  self * 17681 + 37.7 - 26.7
+  
+  c5(:"with ...c5") { self * 2258 + 40.2 - 29.3 }.Bg2.Nc6.O_O.e6.d3.Nf6.Nbd2.Be7.e4.O_O.Re1(:"French Variation")
+  
+  Nf6 { self * 7424 + 40.4 - 24.5 }.Bg2.c6.O_O.Bg4(:"Yugoslav Variation")
+  Bg4 { self * 1773 + 31.6 - 28.9 }.Bg2.Nd7(:"Keres Variation")
+  g6  { self * 1309 + 32.2 - 27.5 }.Bg2.Bg7.O_O.e5.d3.Ne7(:"Pachman System")
+  
+  c6    * 4208 + 31.5 - 26.1
+  Nc6() * 1100 + 43.6 - 30.3
 end
