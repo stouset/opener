4b9906d7cf5dc8c5ba7fba1abf47df2a366b36e5
Stephen Touset <stephen@touset.org>
2009-03-27 11:38:17 -0400

    Cleanup

--- eco/a09.rb
+++ eco/a09.rb
@@ -1,4 +1,4 @@
-Nf3().d5.c4("Reti Opening")
-Nf3().d5.c4.d4(:"Advance Variation")
-Nf3().d5.c4.dxc4("Reti accepted")
-Nf3().d5.c4.dxc4.e3.Be6(:"Keres Variation")
+Nf3().d5.c4 do
+  d4(:"Advance Variation")
+  dxc4("Reti accepted").e3.Be6(:"Keres Variation")
+end
