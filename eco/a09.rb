40619fb8c427f67020c00655e823da4b2e533293
Stephen Touset <stephen@touset.org>
2009-03-27 12:18:00 -0400

    Incorporate information from Wikibooks and chess365.com

--- eco/a09.rb
+++ eco/a09.rb
@@ -1,4 +1,13 @@
 Nf3().d5.c4 do
-  d4(:"Advance Variation")
-  dxc4("Reti accepted").e3.Be6(:"Keres Variation")
+  self * 12200 + 39.6 - 26.0 | '='
+  
+  d4(:"Advance Variation") * 1520 + 32.8 - 36.5
+  dxc4("Réti Accepted")    *  928 + 43.2 - 22.3
+    
+  dxc4.e3.Be6(:"Keres Variation")
+  dxc4.Na3().c5.Nxc4.Nc6.g3.f6 | '='
+  d4.g3.Nc6.Bg2.e5.O_O.Nf6     | '+/='
+  
+  ~ c6.d4 # -> Slav Defense
+  ~ e6    # -> English Opening
 end
