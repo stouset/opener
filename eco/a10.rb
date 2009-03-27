bb4bcfa7f5c7183a085c75864dd4cab858ceb169
Stephen Touset <stephen@touset.org>
2009-03-27 13:56:34 -0400

    Stats from 365chess.com

--- eco/a10.rb
+++ eco/a10.rb
@@ -1,5 +1,18 @@
 c4("English Opening") do
-  g6.e4.e5(:"Adorjan defense")
-  b5(:"Jaenisch gambit")
-  f5(:"Anglo-Dutch defense")
+  self * 161516 + 37.7 - 29.5
+  
+  g6 do
+    self * 12331 + 33.2 - 32.9
+    
+    Nc3 { self * 6164 + 31.8 - 35.2 }.Bg7
+    g3  { self * 2896 + 32.1 - 33.3 }.Bg7
+    d4  { self * 1633 + 40.2 - 27.3 }.Bg7
+    Nf3 { self *  855 + 30.5 - 33.7 }.Bg7
+    e4  { self *  726 + 37.2 - 22.6 }.Bg7
+    
+    e4.e5(:"Adorjan Defense")
+  end
+  
+  f5(:"Anglo-Dutch Defense") * 6235 + 38.6 - 31.8
+  b5(:"Jaenisch Gambit")     *   47 + 38.3 - 46.8
 end
